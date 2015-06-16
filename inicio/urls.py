b975cff383292fa2ab9438b2fc66c13a9bfc0e52
yusnelvy <yusnelvy@gmail.com>
2015-06-16 10:34:46 -0430

    Actualización 16-06-15
    agregar views editar sexo y estado civil en cliente, agregar inicio de
    sesión y permisos de usuario en cotización, limitar por rango los campos
    integer ejemplo colocar un limite al ingresar el numero max o minimo de
    un valor

--- inicio/urls.py
+++ inicio/urls.py
@@ -5,9 +5,14 @@ Pendiente de documentación
 """
 
 from django.conf.urls import patterns, url
-
+from django.contrib.auth.views import login, logout
 from inicio import views
+
 
 urlpatterns = patterns('',
                        url(r'^$', views.pantalla_inicial, name='pantalla_inicial'),
-                       url(r'^index/$', views.pantalla_inicial, name='pantalla_inicial'),)
+                       url(r'^index/$', views.pantalla_inicial, name='pantalla_inicial'),
+                       url(r'^signup$', 'inicio.views.signup', name='signup'),
+                       url(r'^login$', login, {'template_name': 'login/login.html', }, name="login"),
+                       url(r'^home$', 'inicio.views.home', name='home'),
+                       url(r'^logout$', logout, {'template_name': 'inicio/base_menu.html', }, name="logout"),)
--- inicio/views.py
+++ inicio/views.py
@@ -3,7 +3,14 @@
     # pendiente por elaborar
 """
 
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from django.http.response import HttpResponseRedirect
+from django.template.context import RequestContext
+from .forms import SignUpForm
+
+from django.contrib.auth.decorators import login_required
 
 
 def pantalla_inicial(request):
@@ -12,3 +19,40 @@ def pantalla_inicial(request):
     #
 
     return render(request, 'inicio/base_menu.html')
+
+
+def signup(request):
+    if request.method == 'POST':  # If the form has been submitted...
+        form = SignUpForm(request.POST)  # A form bound to the POST data
+        if form.is_valid():  # All validation rules pass
+
+            # Process the data in form.cleaned_data
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            email = form.cleaned_data["email"]
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+
+            # At this point, user is a User object that has already been saved
+            # to the database. You can continue to change its attributes
+            # if you want to change other fields.
+            user = User.objects.create_user(username, email, password)
+            user.first_name = first_name
+            user.last_name = last_name
+
+            # Save new user attributes
+            user.save()
+
+            return HttpResponseRedirect(reverse('uinicio:pantalla_inicial'))  # Redirect after POST
+    else:
+        form = SignUpForm()
+
+    data = {
+        'form': form,
+    }
+    return render_to_response('inicio/signup.html', data, context_instance=RequestContext(request))
+
+
+@login_required()
+def home(request):
+    return render_to_response('inicio/home.html', {'user': request.user}, context_instance=RequestContext(request))
